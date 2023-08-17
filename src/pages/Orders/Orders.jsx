20cc1b75435a8995119edcabc37c77a5f61fe355
Rodrigo Ruiz <rodrigo.ruiz@deskmanager.app>
2023-08-17 16:43:44 -0600

    formulario para agregar orden

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -1,84 +1,129 @@
-import React from 'react';
-import './style.css'
-import { Form, Button, Input, Select, Col, Row } from "antd";
-import {useOrder} from '../../context/orders-context';
-import { burguers } from '../../config/const';
-const OrdersPage = (props) => {
-  const { getOrder, saveOrder } = useOrder()
+import React, { useState } from "react";
+import "./style.css";
+import { Form, Button, Input, Select, Col, Row, InputNumber } from "antd";
+import { useOrder } from "../../context/orders-context";
+import { burguers, typeProduct } from "../../config/const";
+import { MinusOutlined, PlusOutlined } from "@ant-design/icons";
+const OrdersPage = ({ closeForm }) => {
+  const { getOrder, saveOrder } = useOrder();
   const [form] = Form.useForm();
+  const [quantity, setQuantity] = useState(1);
+
+  const _add = (amount) => {
+    const newAmount = quantity + amount;
+    setQuantity(newAmount);
+  };
+
+  const _save = async () => {
+    try {
+      const values = await form.validateFields();
+      await saveOrder(values);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className=''>
+    <div className="">
       <h2>Página de Pedidos</h2>
       <Form form={form}>
-      <Row gutter={16}>
-      <Col span={8}>
-          <Form.Item label="Producto" name="product"
-            rules={[
-              {
-                required: true,
-                message: "Por selecciona una categoria",
-              },
-            ]}
-          >
-            <Select
-              options={[
-                { value: '-1', label: 'Selecciona una opción', disabled: true},
-                ...burguers
+        <Row gutter={16}>
+          <Col span={24}>
+            <Form.Item
+              label="Categoría"
+              name="category"
+              rules={[
+                {
+                  required: true,
+                  message: "Por selecciona una categoria",
+                },
               ]}
-            />
-          </Form.Item>
-        </Col>  
-       
-      </Row>
-      <Row>
-        <Col span={24}>
-          <Form.Item label="Descripción" name="description">
-            <Input />
-          </Form.Item>
-        </Col>
-      </Row>
-      <Row gutter={16}>
-       
-        <Col span={8}>
-          <Form.Item
-            label="Precio"
-            name="price"
-            rules={[
-              {
-                required: true,
-                type: "number",
-                message: "Por favor escribe el precio del producto",
-              },
-            ]}
-          >
-            <Input />
-           </Form.Item>
-        </Col>
-        <Col span={8}>
-          <Form.Item
-            label="Cantidad"
-            name="quantity"
-            rules={[
-              {
-                required: true,
-                type: "number",
-                message: "Por favor escribe cuantos del producto",
-              },
-            ]}
-          >
-            <Input />
-           </Form.Item>
-        </Col>
-      </Row>
-      <div className="buttons-container">
-        <Button type="primary" htmlType="submit" onClick={()=> {alert('Hola')}}>
-          Guardar y salir
-        </Button>
-        <Button htmlType="reset" onClick={() => alert('adios')}>
-          Guardar y nuevo
-        </Button>
-      </div>
-    </Form>
+            >
+              <Select
+                options={[
+                  {
+                    value: "-1",
+                    label: "Selecciona una opción",
+                    disabled: true,
+                  },
+                  ...typeProduct,
+                ]}
+              />
+            </Form.Item>
+          </Col>
+          <Col span={24}>
+            <Form.Item
+              label="Producto"
+              name="product"
+              rules={[
+                {
+                  required: true,
+                  message: "Por selecciona una categoria",
+                },
+              ]}
+            >
+              <Select
+                options={[
+                  {
+                    value: "-1",
+                    label: "Selecciona una opción",
+                    disabled: true,
+                  },
+                  ...burguers,
+                ]}
+              />
+            </Form.Item>
+          </Col>
+          <Col span={24}>
+            <Form.Item
+              label="Nota"
+              name="note"
+              rules={[
+                {
+                  type: "text",
+                  message: "Por favor agrega una nota",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={8}>
+            <Form.Item
+              label="Cantidad"
+              name="quantity"
+              rules={[
+                {
+                  required: true,
+                  message: "Por favor escribe cuantos del producto",
+                },
+              ]}
+            >
+              <InputNumber
+                min={1}
+                max={10}
+                value={quantity}
+                onChange={(value) => setQuantity(value)}
+              />
+              <Button onClick={() => _add(-1)} disabled={quantity <= 1}>
+                <MinusOutlined />
+              </Button>
+              <Button onClick={() => _add(1)}>
+                <PlusOutlined />
+              </Button>
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <div className="buttons-container">
+          <Button type="primary" htmlType="submit" onClick={_save}>
+            Guardar pedido
+          </Button>
+          <Button htmlType="reset" onClick={() => alert("adios")}>
+            Ver detalle del pedido
+          </Button>
+        </div>
+      </Form>
     </div>
   );
 };
